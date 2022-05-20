eef1513f7e5103e18b0a4000d2c3d7262cedbe6d
devika1700 <devikaanair1700@gmail.com>
2022-05-20 09:10:25 +0530

    Initial Commit

--- app/src/main/java/com/example/programmingknowledge/image_to_text/cat.java
+++ app/src/main/java/com/example/programmingknowledge/image_to_text/cat.java
@@ -1,5 +1,6 @@
 package com.example.programmingknowledge.image_to_text;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
@@ -13,7 +14,7 @@ import java.util.Locale;
 public class cat extends AppCompatActivity implements View.OnClickListener{
 
 
-    ImageView image1,image2,image3,image4;
+    ImageView image1,image2,image3,image4,image5,image6;
     TextToSpeech textToSpeech;
     MediaPlayer playNoise;
     MediaPlayer playNoise2;
@@ -27,10 +28,14 @@ public class cat extends AppCompatActivity implements View.OnClickListener{
         image2=findViewById(R.id.imageView11);
         image3=findViewById(R.id.imageView12);
         image4=findViewById(R.id.imageView13);
+        image5=findViewById(R.id.imageView15);
+        image6=findViewById(R.id.imageView14);
         image1.setOnClickListener(this);
         image2.setOnClickListener(this);
         image3.setOnClickListener(this);
         image4.setOnClickListener(this);
+        image5.setOnClickListener(this);
+        image6.setOnClickListener(this);
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int i) {
@@ -73,6 +78,14 @@ public class cat extends AppCompatActivity implements View.OnClickListener{
                     playNoise3= MediaPlayer.create(this,R.raw.t);
                 }
                 playNoise3.start();
+                break;
+
+            case R.id.imageView15:
+                startActivity(new Intent(this,bus.class));
+                break;
+
+            case R.id.imageView14:
+                startActivity(new Intent(this,phonics.class));
                 break;
         }
     }
--- app/src/main/java/com/example/programmingknowledge/image_to_text/phonics.java
+++ app/src/main/java/com/example/programmingknowledge/image_to_text/phonics.java
@@ -1,16 +1,18 @@
 package com.example.programmingknowledge.image_to_text;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class phonics extends AppCompatActivity{
+public class phonics extends AppCompatActivity implements View.OnClickListener{
 
 
-    ImageView image1,image2,image3,image4;
+    Button btn1,btn2,btn3,image4;
     TextToSpeech textToSpeech;
     MediaPlayer playNoise;
     MediaPlayer playNoise2;
@@ -20,10 +22,32 @@ public class phonics extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phonics);
-
-
+        btn1=(Button) findViewById(R.id.learn_btn);
+        btn2=(Button) findViewById(R.id.practice_btn);
+        btn3=(Button) findViewById(R.id.home_btn3);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
     }
+    public void onClick(View v) {
 
+        switch (v.getId()) {
+
+            case R.id.learn_btn:
+                startActivity(new Intent(this,level.class));
+                break;
+
+
+            case R.id.practice_btn:
+                startActivity(new Intent(this,pract1.class));
+                break;
+
+
+            case R.id.home_btn3:
+                startActivity(new Intent(this,first.class));
+                break;
+        }
+    }
 
 
 
